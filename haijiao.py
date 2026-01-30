5fc78c2c1fcead024c3d87975ee7121b856d7138
YohannQin <meyongheng@163.com>
2026-01-30 12:37:44 +0800

    Add files via upload

--- haijiao.py
+++ haijiao.py
@@ -1,45 +1,44 @@
 #coding=utf-8
 #!/usr/bin/python
 import sys
-import json
-import os
 sys.path.append('..')
 from base.spider import Spider
 
 class Spider(Spider):
-    def init(self,extend=""):
+	def init(self,extend=""):
 		self.base_url='http://api.hclyz.com:81/mf'
-		self.json_file = "/storage/emulated/0/111/haijiao/conf/hjbox_media.json"
 		self.raw_json = "https://ghfast.top/https://raw.githubusercontent.com/YohannQin/my_test/refs/heads/main/my_video.json"
-        
+
 	def homeContent(self,filter):
 		classes = [{"type_name": "博主","type_id":"author"}]
 		result = {"class": classes}
 		return result
 
 	def categoryContent(self,tid,pg,filter,extend):
+		home = self.fetch(f'{self.base_url}/json.txt').json()
+		data = home.get("pingtai")[1:]
 		videos = [
 			{
-				"vod_id": "/12233",
-				"vod_name": "55555",
+				"vod_id": "/12345",
+				"vod_name": "6666",
 				"vod_pic": "",
-				"vod_remarks": "6666",
+				"vod_remarks": "5555",
 				"style": {"type": "rect", "ratio": 1.33}
 			}
 		]
-
-        result = {
-            "page": pg,
-            "pagecount": 1,
-            "limit": len(videos),
-            "total": len(videos),
-            "list": videos
-        }
+		data = self.fetch(self.raw_json).json()
+		self.log(data)
+		videos = data['list']
+		result = {
+			"page": pg,
+			"pagecount": 1,
+			"limit": len(videos),
+			"total": len(videos),
+			"list": videos
+		}
 		return result
 
 	def detailContent(self,array):
-	    pass
-	"""
 		id = array[0]
 		data = self.fetch(f'{self.base_url}/{id}').json()
 		zhubo = data['zhubo']
@@ -51,7 +50,6 @@ class Spider(Spider):
 		}]
 		result = {"list": vod}
 		return result
-		"""
 
 	def playerContent(self,flag,id,vipFlags):
 		result = {
